9cc96cde3ba7c9622031b5da3e220e4f10e0c7de
Deepika <deepibhilwarainfotech@gmail.com>
2019-05-03 16:44:08 +0530

    ionicproject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
 import { Platform } from '@ionic/angular';
+import {NotificationService} from '../app/notification.service';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -12,15 +14,39 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
+    private fcm: NotificationService,
+    public toastController: ToastController,
     private statusBar: StatusBar
   ) {
     this.initializeApp();
+  }
+
+  private async presentToast(message) {
+    const toast = await this.toastController.create({
+      message,
+      duration: 3000
+    });
+    toast.present();
+  }
+
+
+  private notificationSetup() {
+    this.fcm.getToken();
+    this.fcm.onNotifications().subscribe(
+      (msg) => {
+        if (this.platform.is('ios')) {
+          this.presentToast(msg.aps.alert);
+        } else {
+          this.presentToast(msg.body);
+        }
+      });
   }
 
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.notificationSetup();
     });
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,39 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { Firebase } from '@ionic-native/firebase/ngx';
+
+const config = {
+  apiKey: "AIzaSyDQXjQd_Lnp9GrEptyVLaqqiyxvj83G560",
+  authDomain: "ionicproject-cefdb.firebaseapp.com",
+  databaseURL: "https://ionicproject-cefdb.firebaseio.com",
+  projectId: "ionicproject-cefdb",
+  storageBucket: "ionicproject-cefdb.appspot.com",
+  messagingSenderId: "509332959018"
+};
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [
+    BrowserModule, 
+    IonicModule.forRoot(), 
+    AppRoutingModule,
+    AngularFireModule.initializeApp(config),
+    AngularFirestoreModule]
+    ,
   providers: [
     StatusBar,
     SplashScreen,
+    BarcodeScanner,
+    Firebase,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
